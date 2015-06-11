efc36c93c8a33f40717e91463f9779b9d400cc33
loganoconnell <logan.developeremail@gmail.com>
2015-06-11 15:33:45 -0400

    loganoconnell
    Personal Website

--- js/contact-script.js
+++ js/contact-script.js
@@ -166,4 +166,19 @@
 
 		})();
 
+		if (navigator.userAgent.match(/iPhone;.*CPU.*OS 8_\d/i) && window.innerHeight != document.documentElement.clientHeight) {
+  var fixViewportHeight = function() {
+    document.documentElement.style.height = window.innerHeight + "px";
+    if (document.body.scrollTop !== 0) {
+      window.scrollTo(0, 0);
+    }
+  }.bind(this);
+
+  window.addEventListener("scroll", fixViewportHeight, false);
+  window.addEventListener("orientationchange", fixViewportHeight, false);
+  fixViewportHeight();
+
+  document.body.style.webkitTransform = "translate3d(0,0,0)";
+}
+
 })();
