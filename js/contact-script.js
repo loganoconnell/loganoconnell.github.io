751c558c691f85580f3f9be2b7191d07d7a7a7ad
loganoconnell <logan.developeremail@gmail.com>
2015-06-14 10:32:12 -0400

    loganoconnell
    Personal Website

--- js/contact-script.js
+++ js/contact-script.js
@@ -182,4 +182,19 @@
 
 		})();
 
+
+		$(document)
+		.one('focus.textarea', '.autoExpand', function(){
+			var savedValue = this.value;
+			this.value = '';
+			this.baseScrollHeight = this.scrollHeight;
+			this.value = savedValue;
+		})
+		.on('input.textarea', '.autoExpand', function(){
+			var minRows = this.getAttribute('data-min-rows')|0,rows;
+			this.rows = minRows;
+			rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+			this.rows = minRows + rows;
+		});
+
 })();
