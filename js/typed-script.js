1e9b1b23f6c879bf42d778ed8b8d80035de3be5c
Logan O'Connell <logan.developeremail@gmail.com>
2016-05-29 13:48:37 -0400

    loganoconnell
    Personal Website

---
+++ js/typed-script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".tl").typed({
+        strings: ["Developer.", "Innovative.", "Professional.", "Minimalist.", "Productive.", "Consistent.", "Original.", "Reliable.", "Ambitious.", "Passionate.", "Diligent.", "Driven.", "Dedicated.", "Dependable."],
+        typeSpeed: 1,
+        backSpeed: 1,
+        backDelay: 2000,
+        loop: true,
+        showCursor: false
+    });
+});
+
+$(function(){
+    $(".body").typed({
+    	strings: ["Hello, ^500 my name is Logan. ^1000 I'm a self-taught developer that strives for simplicity, ^500 minimalism, ^500 and professionalism. ^1000 When I'm not tinkering with code or skimming open-source, ^500 I'm usually hanging with my friends or singing to loud music. ^1000 <br><br> If you'd like to get in touch, ^500 feel free to drop me a line. ^1000 <br><br> email// <a href=mailto:you@loganoconnell.me>you@loganoconnell.me</a> ^500 <br> twitter// <a href=https://twitter.com/logandev22>@logandev22</a> ^500 <br> github// <a href=https://github.com/loganoconnell>@loganoconnell</a>"],
+    	showCursor: false
+    });
+});
